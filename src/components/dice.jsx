9579876ee6d1b6715f2398aa54d92e3055127f5d
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2024-04-12 22:44:11 +0200

    dice faces added

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -3,10 +3,16 @@ export default function Dice(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF",
   };
-  
+  const diceClass = `box dice-${props.value}`;
+  const diceFace = [];
+  for (let i = 0; i < props.value; i++) {
+    diceFace.push(<span className="dot"></span>);
+  }
+
   return (
-    <div style={styles} className="box" onClick={props.holdDice}>
-      <h2>{props.value}</h2>
+    <div style={styles} className={diceClass} onClick={props.holdDice}>
+      {/* <h2>{props.value}</h2> */}
+      {diceFace}
     </div>
   );
 }
